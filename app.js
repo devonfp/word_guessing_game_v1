2e3f437e388ab71a4475dfaa7417865578381490
Devon Poston <devonposton1@gmail.com>
2022-09-27 10:34:21 -0400

    Major fixes

--- app.js
+++ app.js
@@ -6,59 +6,90 @@ const startGame = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
 let missed = '0';
 
-const phrases = [ 'hello there', ' ', 'nice to', ' ', 'meet you', ' ', 'I hope', ' ', 'you have', ' ', 'a lovely day'];
+const phrases = [ 'hello there', 
+                  'have a lovely day'];
 
 startGame.addEventListener('click', (e) => {
- overlay.style.display = 'none';
+   getRandomPhraseAsArray(phrases); 
+   overlay.style.display = 'none';
 });
   
 
 function getRandomPhraseAsArray(arr) {
- const randomPhrase = Math.floor(Math.random() * arr.length);
-const characters =  arr.toString().split('');
-return characters;
+const randomPhrase = Math.floor(Math.random() * arr.length);
+const characters =  arr[randomPhrase].split('');
+addPhraseToDisplay(characters)
 }
-getRandomPhraseAsArray(phrases);
+
 
 function addPhraseToDisplay(arr) {
- const idPhrase = document.getElementById('phrase');
+ //const idPhrase = document.getElementById('phrase');
  const phraseUl = document.querySelector('ul');
 
 
-    for (characters in arr) {
+    arr.forEach (character => {
         const li = document.createElement('li');
-        li.textContent = characters;
-        idPhrase.appendChild(li);
-        li.classList.add('letter');
- }
+        li.textContent = character;
+
+        if(li.textContent === ' ') {
+         li.classList.add('space');
+    } else {
+      li.classList.add('letter');
+    }
+        phraseUl.appendChild(li);
+ })
 };
-const phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray); 
+//const phraseArray = getRandomPhraseAsArray(phrases);
+//addPhraseToDisplay(phraseArray); 
 
 
 
 function checkLetter(button) {
- let letter = document.getElementsByClassName('letter');
-   const lis = document.querySelectorAll('li');
+ let letters = document.getElementsByClassName('letter');
     let match = null; 
-    for (letter in lis) {
-       if (button.textContent === lis.textContent ) {
-        lis.classList.add('show');
+    for (letters in button) {
+       if (button.textContent === letters.textContent ) {
+        letters.classList.add('show');
         button.textContent.match; 
-        return match; 
+        return match;
+      } else {
+        const replaceImage = document.getElementsByClassName('tries');
+        replaceImage[0].innerHTML = '<li class="tries"><img src="images/lostHeart.png" height="35px" width="30px"></li>'
+        missed++     
       }
-   }
+   }   
  }; 
 
  qwerty.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
-   checkLetter(e.target);
-   qwerty.classList.add('chosen'); 
-    } else {
 
+    const btn = e.target;
+
+    if (btn.tagName === 'BUTTON') {
+   checkLetter(btn);
+   btn.classList.add('chosen'); 
+   btn.disabled = true;
     }
- });  
+ }); 
 
+
+
+ function checkWin() {
+   const letter = document.querySelectorAll('.letter li');
+   const show = document.querySelectorAll('.show li');
+   const headline = document.getElementsByClassName("header")
+
+    if (show.length === letter.length) {
+       overlay.classList.add('win');
+       headline.textContent = 'Congrats, you win!';
+       overlay.style.display = 'flex';
+    }
+
+   if (missed > 4) {
+      overlay.classList.add('lose');
+      headline.textContent = 'Sorry, you lose.';
+      overlay.style.display = 'flex';
+   }
+}
 
 
 
